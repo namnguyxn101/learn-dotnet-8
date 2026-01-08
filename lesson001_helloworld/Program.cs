84fab17230765f3945cf84ce9d729cee33bb12cb
namnguyxn101 <phuongnam3235@gmail.com>
2026-01-08 20:10:32 +0700

    lesson001: hello world

---
+++ lesson001_helloworld/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Demo;
+
+namespace lesson001_helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*F
+            Comment block
+            */
+
+            // Comment line
+
+            Console.WriteLine("Hello world");
+        }
+
+        /// <summary>
+        /// Phuong thuc tra ve tong cua 2 so nguyen
+        /// </summary>
+        /// <param name="a">so nguyen 1</param>
+        /// <param name="b">so nguyen 2</param>
+        /// <returns>Tong a + b</returns>
+        static int Tong(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
+
+namespace Demo
+{
+    class Abc
+    {
+        static void Greet()
+        {
+            Console.WriteLine("Welcome to C#");
+        }
+    }
+}
