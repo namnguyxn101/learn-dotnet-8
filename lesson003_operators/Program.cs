1352126434225377b1ad974663e7777e26e3ff6d
namnguyxn101 <phuongnam3235@gmail.com>
2026-01-09 20:32:34 +0700

    lesson003: operators

---
+++ lesson003_operators/Program.cs
@@ -0,0 +1,39 @@
+﻿// int a = 10, b = 3;
+// Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+// Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+// Console.WriteLine("{0} * {1} = {2}", a, b, a * b);
+// Console.WriteLine("{0} / {1} = {2}", a, b, (float)a / b);
+// Console.WriteLine("{0} % {1} = {2}", a, b, a % b);
+//==============================================
+
+{
+    int c = 10;
+    c += 2;
+    Console.WriteLine(c);
+}
+{
+    int c = 10;
+    c -= 2;
+    Console.WriteLine(c);
+}
+{
+    int c = 10;
+    c *= 2;
+    Console.WriteLine(c);
+}
+{
+    int c = 10;
+    c /= 2;
+    Console.WriteLine(c);
+}
+{
+    int c = 10;
+    c %= 2;
+    Console.WriteLine(c);
+}
+//==============================================
+
+// int x = 5, y = 5;
+// Console.WriteLine(x++); // 5
+// Console.WriteLine(x); // 6
+// Console.WriteLine(++y); // 6
