15062438760ebf7444c1d2ec4c6883efcd3499b3
namnguyxn101 <phuongnam3235@gmail.com>
2026-01-10 11:00:24 +0700

    lesson013: math

---
+++ lesson013_math/Program.cs
@@ -0,0 +1,25 @@
+﻿// Hằng số
+// Console.WriteLine($"Hang so PI: {Math.PI}, E: {Math.E}");
+
+// Max, Min
+// Console.WriteLine($"Max {Math.Max(5, 6)}");
+// Console.WriteLine($"Min {Math.Min(9, 1)}");
+
+// Abs
+// Console.WriteLine($"/-3/ = {Math.Abs(-3)}");
+
+// Sign
+// Console.WriteLine($"10 => {Math.Sign(10)}");
+// Console.WriteLine($"0 => {Math.Sign(0)}");
+// Console.WriteLine($"-2 => {Math.Sign(-2)}");
+
+// Lượng giác
+// Sin, Cos, Tan, Asin, Acos, Atan
+
+// Math.Sqrt(a), Math.Pow(a, b), Math.Log(a), Math.Log10(a)
+
+// Làm tròn
+// Math.Round(a) (5.6 ==> 6; 5.4 ==> 5)
+// Math.Ceiling(a) (3.3, 3.6, 3.1 ==> 4)
+// Math.Floor(a) (2.2, 2.9, 2.6 ==> 2)
+// Math.Truncate(a) (3.141592654 ==> 3)
