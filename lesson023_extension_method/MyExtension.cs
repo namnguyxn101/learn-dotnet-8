351504af59fd9c96835834fcf0580996d2d495df
namnguyxn101 <phuongnam3235@gmail.com>
2026-01-11 14:19:23 +0700

    lesson023: extension method

---
+++ lesson023_extension_method/MyExtension.cs
@@ -0,0 +1,8 @@
+namespace MyLib
+{
+    public static class Xyz
+    {
+        public static double BinhPhuong(this double x) => x * x;
+        public static double CanBacHai(this double x) => Math.Sqrt(x);
+    }
+}
---
+++ lesson023_extension_method/Program.cs
@@ -0,0 +1,30 @@
+﻿using MyLib;
+
+// Extension method
+// Lop tinh static
+static class Abc
+{
+    // Muốn mở rộng phương thức cho lớp nào thì tham số đầu tiên phải là đối tượng của lớp đó.
+    // Thêm từ khóa this.
+    public static void Print(this string s, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(s);
+        Console.ResetColor();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // string s = "Xin chao cac ban";
+        // s.Print(ConsoleColor.Green);
+        // s.Print(ConsoleColor.Red);
+        //=============================================
+
+        double a = 2.5;
+        Console.WriteLine(a.BinhPhuong());
+        Console.WriteLine(a.CanBacHai());
+    }
+}
