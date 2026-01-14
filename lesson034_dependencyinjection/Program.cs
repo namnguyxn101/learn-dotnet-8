0ed11290c353496209e10e9fa504054ef32ebfb2
namnguyxn101 <phuongnam3235@gmail.com>
2026-01-14 22:11:03 +0700

    lesson034: dependency injection (direct reference to dependency)

---
+++ lesson034_dependencyinjection/Program.cs
@@ -0,0 +1,44 @@
+﻿// Dependency - Phụ thuộc
+// Thiết kế kiểu truyền thống - tham chiếu trực tiếp đến Dependency
+class ClassC
+{
+    public void ActionC() => Console.WriteLine("Action in ClassC");
+}
+
+class ClassB
+{
+    // Phụ thuộc của ClassB là ClassC
+    ClassC c_dependency;
+
+    public ClassB(ClassC classc) => c_dependency = classc;
+    public void ActionB()
+    {
+        Console.WriteLine("Action in ClassB");
+        c_dependency.ActionC();
+    }
+}
+
+class ClassA
+{
+    // Phụ thuộc của ClassA là ClassB
+    ClassB b_dependency;
+
+    public ClassA(ClassB classb) => b_dependency = classb;
+    public void ActionA()
+    {
+        Console.WriteLine("Action in ClassA");
+        b_dependency.ActionB();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ClassC objectC = new ClassC();
+        ClassB objectB = new ClassB(objectC);
+        ClassA objectA = new ClassA(objectB);
+
+        objectA.ActionA();
+    }
+}
