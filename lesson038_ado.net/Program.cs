e47e6652c99e1080b838437534ae83bddc009a9f
namnguyxn101 <phuongnam3235@gmail.com>
2026-01-19 15:45:10 +0700

    lesson038: ado.net (SqlParameter, ExecuteReader, ExecuteScalar, ExecuteNonQuery)

--- lesson038_ado.net/Program.cs
+++ lesson038_ado.net/Program.cs
@@ -8,32 +8,68 @@ class Program
     {
         // string sqlConnection = "Data Source=localhost,1433;Initial Catalog=xtlab;User ID=sa;Password=Password123;TrustServerCertificate=True";
 
-        var sqlStringBuilder = new MySqlConnectionStringBuilder();
-        sqlStringBuilder["Server"] = "localhost";
-        sqlStringBuilder["Port"] = "3307";
+        var sqlStringBuilder = new SqlConnectionStringBuilder();
+        sqlStringBuilder["Server"] = "localhost,1433";
         sqlStringBuilder["Database"] = "xtlab";
-        sqlStringBuilder["UID"] = "root";
-        sqlStringBuilder["PWD"] = "abc123";
-        // sqlStringBuilder["TrustServerCertificate"] = true;
+        sqlStringBuilder["UID"] = "sa";
+        sqlStringBuilder["PWD"] = "Password123";
+        sqlStringBuilder["TrustServerCertificate"] = true;
 
         string sqlConnection = sqlStringBuilder.ToString();
 
-        using var conn = new MySqlConnection(sqlConnection);
+        using var conn = new SqlConnection(sqlConnection);
 
         // Console.WriteLine(conn.State);
 
         conn.Open();
 
-        using DbCommand command = new MySqlCommand();
+        using var command = new SqlCommand();
         command.Connection = conn;
-        command.CommandText = "SELECT * FROM Sanpham LIMIT 0, 10;";
+        command.CommandText = "SELECT * FROM Danhmuc WHERE DanhmucID >= @DanhmucID;";
 
-        var dataReader = command.ExecuteReader();
+        // var danhMucId = new SqlParameter("@DanhmucID", 5);
+        // command.Parameters.Add(danhMucId);
+        // // Gán lại giá trị khác
+        // danhMucId.Value = 3;
+        // ---> Gọn hơn
+        var danhMucId = command.Parameters.AddWithValue("@DanhmucID", 5);
+        danhMucId.Value = 3;
+
+        // =-=-=-=- ExecuteReader : Dùng khi cần kết quả trả về nhiều dòng -=-=-=-=
+        // using var dataReader = command.ExecuteReader();
+
+        // if (dataReader.HasRows)
+        // {
+        //     while (dataReader.Read())
+        //     {
+        //         // Nếu biết type của field và thứ tự thì dùng (Theo thứ tự):
+        //         var id = dataReader.GetInt32(0);
+
+        //         // Nếu biết tên của field thì dùng:
+        //         var tenDanhMuc = dataReader["TenDanhMuc"];
+
+        //         // Nếu chỉ biết thứ tự thì có thể dùng:
+        //         var moTa = dataReader[2];
+
+        //         Console.WriteLine($"{id} - {tenDanhMuc} - {moTa}");
+        //     }
+        // }
+        // else
+        // {
+        //     Console.WriteLine("No data");
+        // }
+
+        // =-=-=-=- ExecuteScalar : Trả về 1 giá trị (row 1, column 1) -=-=-=-=
+        // var returnValue = command.ExecuteScalar();
+        // Console.WriteLine(returnValue);
+
+        // =-=-=-=- ExecuteNonQuery : Trả về số dòng bị ảnh hưởng (Insert, Update, Delete) -=-=-=-=
+        command.CommandText = "INSERT INTO Shippers (Hoten, Sodienthoai) VALUES (@Hoten, @Sodienthoai)";
+        command.Parameters.AddWithValue("@Hoten", "Abc");
+        command.Parameters.AddWithValue("@Sodienthoai", 123456);
         
-        while (dataReader.Read())
-        {
-            Console.WriteLine($"{dataReader["TenSanpham"], 30} - Gia: {dataReader["Gia"]}");
-        }
+        var kq = command.ExecuteNonQuery();
+        Console.WriteLine($"So dong bi anh huong: {kq}");
 
         conn.Close();
     }
