4d965927e95c60b025c16fd4225524da236d7014
namnguyxn101 <phuongnam3235@gmail.com>
2026-01-28 13:58:29 +0700

    lesson045: request info, encoding, cookies, json, form (cookies)

--- lesson045_requestinfo_encoding_cookies_json_form/Program.cs
+++ lesson045_requestinfo_encoding_cookies_json_form/Program.cs
@@ -36,9 +36,43 @@ app.MapGet("/encoding", async (context) =>
     await context.Response.WriteAsync("Encoding");
 });
 
-app.MapGet("/cookies", async (context) =>
+app.MapGet("/cookies/{*action}", async (string? action, HttpContext context) =>
 {
-    await context.Response.WriteAsync("Cookies");
+    var menu = HtmlHelper.MenuTop(
+        HtmlHelper.DefaultMenuTopItems(),
+        context.Request
+    );
+
+    // var action = context.GetRouteValue("action") ?? "read";
+    action ??= "read";
+
+    string message = "";
+
+    // if (action.ToString() == "write")
+    if (action == "write")
+    {
+        var option = new CookieOptions()
+        {
+            Path = "/",
+            Expires = DateTime.Now.AddDays(1)  
+        };
+        
+        context.Response.Cookies.Append("MaSanPham", "1lk2nvkfsajo234lk", option);
+        message = "Cookie duoc ghi";
+    }
+    else
+    {
+        var listcokie = context.Request.Cookies.Select((header) => $"{header.Key}: {header.Value}".HtmlTag("li"));
+        message = string.Join("", listcokie).HtmlTag("ul");
+    }
+
+    var link = "<a href=\"/cookies/read\" class=\"btn btn-danger\">Read cookie</a><a href=\"/cookies/write\" class=\"btn btn-success\">Write cookie</a>";
+    link = link.HtmlTag("div", "container mt-4");
+    message = message.HtmlTag("div", "alert alert-danger");
+
+    var html = HtmlHelper.HtmlDocument($"Cookie: {action}", menu + link + message);
+
+    await context.Response.WriteAsync(html);
 });
 
 app.MapGet("/json", async (context) =>
